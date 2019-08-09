eb966b38aee2dbda1d05881ed80b9d669b08ba92
andrewbastian <andrew.bastian@gmail.com>
2019-08-08 23:03:22 -0700

    added a scroll listerner to the nav bar

--- js/index.js
+++ js/index.js
@@ -42,13 +42,47 @@ aTags.addEventListener('mouseover', (event)=>{
 //     document.getElementById('audio').play();
 //   }
 // });
-var horn = new Audio('audio/bus-horn.wav');
 
-let audio = document.createElement('audio');
-audio.setAttribute("id", "audio");
-let audioFile = document.getElementById('audio');
-audioFile.addEventListener("keydown", (event)=>{
-  if(event.keyCode === 40){
-    horn.play();
+// KEYPRESS audio
+
+// var horn = new Audio('audio/bus-horn.wav');
+//
+// let audio = document.createElement('audio');
+// audio.setAttribute("id", "audio");
+// audio.setAttribute("src", 'audio/bus-horn.wav')
+// audio.setAttribute("type", "audio/wav")
+// // let audioFile = document.querySelector('#audio');
+// // audioFile.src = ("audio/bus-horn.wav");
+// // let playingAudio = document.querySelector('#audio');
+// audio.addEventListener("keypress", (event)=>{
+//   if(event.keyCode === 65){
+//     horn.play();
+//     console.log("playing");
+//   }
+// });
+let intro = document.querySelector(".intro img");
+intro.style.paddingTop = "160px";
+document.querySelector(".main-navigation").setAttribute("id", "navbar");
+let logo = document.querySelector(".logo-heading");
+logo.setAttribute("id","logo");
+logo.style.textShadow = "0px 11px 3px rgba(206,151,197,0.87)";
+logo.addEventListener("keydown", (event)=>{
+  logo.animate({
+    color: ['red','green'],
+    textShadow: ["0px 11px 3px rgba(206,151,197,0.87)", "4px -31px 15px rgba(206,151,197,0.87)", "text-shadow: 0px 4px 0px #CE97C5;","0px 11px 3px rgba(206,151,197,0.87)"],
+    offset: [ 0, 0.8 ],
+  }, 2000);
+}
+)
+animate()
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.padding = "25px 10px";
+    document.getElementById("logo").style.fontSize = "25px";
+  } else {
+    document.getElementById("navbar").style.padding = "80px 10px";
+    document.getElementById("logo").style.fontSize = "40px";
   }
-});
+}
