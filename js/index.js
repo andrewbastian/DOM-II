93d00b11e03a790468713d1593ade5582a349b68
andrewbastian <andrew.bastian@gmail.com>
2019-08-12 16:38:03 -0700

    Realized I didn't make this last commit on saturday. here it is now.

--- js/index.js
+++ js/index.js
@@ -3,69 +3,82 @@
 // 	* [√] `mouseover`
 // 	* [√] `keydown`
 // 	* [√] `wheel`
-// 	* [ ] `drag / drop`
+// 	* [√] `animationstart`
 // 	* [√] `load`
-// 	* [ ] `focus`
-// 	* [ ] `resize`
+// 	* [√] `copy`
+// 	* [√] `online`
 // 	* [√] `scroll`
 // 	* [√] `select`
 // 	* [√] `click`
 
-
+//online log
+window.addEventListener('online', (event) => {
+  console.log("your computer is watching you!!!");
+});
 //fade from black on load:
 var log = document.querySelector('body');
 
 window.addEventListener('load', (event) => {
-    log.animate({
-    backgroundColor: ['black','white'],
-    opacity: [ 0, 0.9, 1 ],
-    offset: [ 0, 0.8 ], // Shorthand for [ 0, 0.8, 1 ]
-    easing: [ 'ease-in', 'ease-out' ],
+  log.animate({
+    backgroundColor: ['black', 'white'],
+    opacity: [0, 0.9, 1],
+    offset: [0, 0.8], // Shorthand for [ 0, 0.8, 1 ]
+    easing: ['ease-in', 'ease-out'],
   }, 2000);
 });
 //scroll nav
-  window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {
+  scrollFunction()
+};
 
-  function scrollFunction() {
-    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-      document.getElementById("navbar").style.padding = "25px 10px";
-      document.getElementById("logo").style.fontSize = "25px";
-    } else {
-      document.getElementById("navbar").style.padding = "80px 10px";
-      document.getElementById("logo").style.fontSize = "40px";
-    }
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.padding = "25px 10px";
+    document.getElementById("logo").style.fontSize = "25px";
+  } else {
+    document.getElementById("navbar").style.padding = "80px 10px";
+    document.getElementById("logo").style.fontSize = "40px";
   }
+}
+
 // BIG LETTER>little letter>BIG LETTER>little letter
 let navItems = document.querySelectorAll('a');
-navItems.forEach((as, i)=>{
-  as.addEventListener("click", (event)=>{
+navItems.forEach((as, i) => {
+  as.addEventListener("click", (event) => {
     event.preventDefault();
-  } )
+  })
 })
 navItems.forEach((aTags, i) => {
-aTags.addEventListener('mouseover', (event)=>{
+  aTags.addEventListener('mouseover', (event) => {
     aTags.animate({
-      fontSize: ["20px", "40px", "30px","20px"],
-      offset: [ 0, 0.8 ],
+      fontSize: ["20px", "40px", "30px", "20px"],
+      offset: [0, 0.8],
     }, 2000);
+    aTags.addEventListener('animationstart', (e) => {
+      console.log('Animation started');
+    });
     logo.animate({
-      color: ['red','green'],
-      textShadow: ["0px 11px 3px rgba(206,151,197,0.87)", "4px -31px 15px rgba(206,151,197,0.87)", "text-shadow: 0px 4px 0px #CE97C5;","0px 11px 3px rgba(206,151,197,0.87)"],
-      offset: [ 0, 0.4,0.8 ],
+      color: ['red', 'green'],
+      textShadow: ["0px 11px 3px rgba(206,151,197,0.87)", "4px -31px 15px rgba(206,151,197,0.87)", "text-shadow: 0px 4px 0px #CE97C5;", "0px 11px 3px rgba(206,151,197,0.87)"],
+      offset: [0, 0.4, 0.8],
     }, 2000);
-  })});
+  })
+});
 
-  let intro = document.querySelector(".intro img");
-  intro.style.paddingTop = "160px";
-  document.querySelector(".main-navigation").setAttribute("id", "navbar");
+let intro = document.querySelector(".intro img");
+intro.style.paddingTop = "160px";
+document.querySelector(".main-navigation").setAttribute("id", "navbar");
 
-  let navs
 
-  let logo = document.querySelector(".logo-heading");
-  logo.setAttribute("id","logo");
-  logo.style.textShadow = "0px 11px 3px rgba(206,151,197,0.87)";
 
-  // Keydown horn
+let logo = document.querySelector(".logo-heading");
+logo.setAttribute("id", "logo");
+logo.style.textShadow = "0px 11px 3px rgba(206,151,197,0.87)";
+
+
+
+
+// Keydown horn
 
 
 let dataKey = document.createElement("kbd");
@@ -103,14 +116,14 @@ function animateBtn(event) {
 
   event.stopPropagation()
   event.currentTarget.animate({
-  backgroundColor: ['green',"blue",'yellow'],
-  opacity: [ 0, 0.9, 1 ],
-  offset: [ 0, 0.8 ], // Shorthand for [ 0, 0.8, 1 ]
-  easing: [ 'ease-in', 'ease-out' ],
-}, 2000);
+    backgroundColor: ['green', "blue", 'yellow'],
+    opacity: [0, 0.9, 1],
+    offset: [0, 0.8], // Shorthand for [ 0, 0.8, 1 ]
+    easing: ['ease-in', 'ease-out'],
+  }, 2000);
 }
 // btns
-let btn= document.querySelectorAll('.btn')
+let btn = document.querySelectorAll('.btn')
 btn[0].setAttribute("id", "btn-1");
 btn[1].setAttribute("id", "btn-2");
 btn[2].setAttribute("id", "btn-3");
@@ -136,15 +149,25 @@ h2Input1.style.marginInlineStart = "0px";
 h2Input1.style.marginInlineEnd = "0px";
 h2Input1.style.display = "block";
 h2Input1.style.border = "none";
-h2Input1.addEventListener('select',(event)=>{
-    h2Input1.animate({
-      color: ["red", "orange", "yellow","orange","red"],
-    }, 3000); });
+h2Input1.addEventListener('select', (event) => {
+  h2Input1.animate({
+    color: ["red", "orange", "yellow", "orange", "red"],
+  }, 3000);
+});
 let h2Input2 = document.createElement("input");
 let h2Input3 = document.createElement("input");
 let h2Input4 = document.createElement("input");
 
-let pS = document.querySelectorAll("p")
+let pS = document.querySelectorAll("p");
+//copy to upper case
+pS.forEach((pTags, i) => {
+  pTags.setAttribute("contenteditable", "true");
+  pTags.addEventListener('copy', (event) => {
+    const selection = document.getSelection();
+    event.clipboardData.setData('text/plain', selection.toString().toUpperCase());
+    event.preventDefault();
+  })
+});
 pS[0].prepend(h2Input1);
 let h2s = document.querySelectorAll('h2');
 h2s[0].style.display = "none";
@@ -154,53 +177,54 @@ h2s[0].style.display = "none";
 //
 
 
-
-//WHEEL
 //WHEEL Func
-function blowUpWheel(event){
+function blowUpWheel(event) {
   event.preventDefault();
 
-    scale += event.deltaY * -0.01;
+  scale += event.deltaY * -0.01;
 
-    // Restrict scale
-    scale = Math.min(Math.max(.01, scale), 2);
+  // Restrict scale
+  scale = Math.min(Math.max(.01, scale), 2);
 
-    // Apply scale transform
-    wheelImg.style.transform = `scale(${scale})`;
-  }
+  // Apply scale transform
+  wheelImg.style.transform = `scale(${scale})`;
+}
 let scale = 1;
 let headImg = document.querySelector("img").setAttribute("id", "head-img");
 let wheelImg = document.querySelector("#head-img");
 wheelImg.addEventListener("wheel", blowUpWheel);
 
-//focus
-let footer = document.querySelector("footer")
-// var x = document.createElement("FORM");
-// function myFunction() {
-//   var x = document.createElement("FORM");
-//   x.setAttribute("id", "myForm");
-//   body.appendChild(x);
+// //focus
+// let footer = document.querySelector("footer")
+// // var x = document.createElement("FORM");
+// // function myFunction() {
+// //   var x = document.createElement("FORM");
+// //   x.setAttribute("id", "myForm");
+// //   body.appendChild(x);
+// //
+// //   var y = document.createElement("INPUT");
+// //   // y.stopPropagation();
+// //   y.setAttribute("type", "text");
+// //   y.setAttribute("value", "Donald");
+// //   document.getElementById("myForm").appendChild(y);
+// // }
+// let form = document.createElement("form");
+// form.setAttribute("name", "fname");
+// form.setAttribute("id", "fname");
+// let formlabel = document.createElement("label");
+// formlabel.setAttribute("for", "fname");
+// formlabel.style.textContent = "Tell Us Your Story"
+// let formInput = document.createElement("input");
+// formInput.setAttribute("type", "text");
+// formInput.setAttribute("id", "formIn");
+// let formIn = document.querySelector("#formIn");
 //
-//   var y = document.createElement("INPUT");
-//   // y.stopPropagation();
-//   y.setAttribute("type", "text");
-//   y.setAttribute("value", "Donald");
-//   document.getElementById("myForm").appendChild(y);
-// }
-let form = document.createElement("form");
-form.setAttribute("name", "fname");
-form.setAttribute("id", "fname");
-let formlabel = document.createElement("label");
-formlabel.setAttribute("for", "fname");
-formlabel.style.textContent = "Tell Us Your Story"
-let formInput = document.createElement("input");
-formInput.setAttribute("type", "text");
-formInput.style.width = "100%";
-formInput.style.fontSize = "25px";
-// form.setAttribute("autocomplete", "on");
-body.append("form");
-form.append('formlabel');
-formlabel.append("formInput")
+// formIn.style.width = "100%";
+// formIn.style.fontSize = "25px";
+// // form.setAttribute("autocomplete", "on");
+// log.append("form");
+// form.append('formlabel');
+// form.append("formIn")
 
 //
 
